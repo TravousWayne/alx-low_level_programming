76248685ae55900c532dbb44adf3651ec8546875
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-08 18:26:22 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Prints a text
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
