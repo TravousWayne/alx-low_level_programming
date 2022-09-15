e06faf01d835c434c36867eb783e2ce88cde4d8e
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-14 17:25:44 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
