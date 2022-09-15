03913f4c4d2e68bcbc271986a96e0a1e41a0d7d6
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-14 19:14:56 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign based on condition
+ * print_sign - function to check for a sign of a master
  * @n: argument passed
  * Return: 1, 0 -1
  */
@@ -9,12 +9,12 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar('+');
+	return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		return (0);
+	_putchar('0');
+	return (0);
 	}
 }
