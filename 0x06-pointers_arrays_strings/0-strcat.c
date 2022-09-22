5060c631dfc93b96ce32bc1fcba3778e0c971796
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-22 15:11:46 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *temp = dest;
+	int index = 0, dest_len = 0;
 
-	while (*dest)
-		dest++;
+	while (dest[index++])
+		dest_len++;
 
-	while (*src)
-		*dest++ = *src++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	*dest = '\0';
-	return (temp);
+	return (dest);
 }
