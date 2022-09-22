31373a4fd8fad9242235682ae130e630d4c3ea0d
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-22 16:03:54 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,13 +3,20 @@
 
 /**
  * _strncat - a function that concatenates two strings
- * @dest: first param
- * @src: second param
- * @n: third param
- * Return: string
+ * @dest: string to be appended upon
+ * @src: string to be completed at the end of dest
+ * @n: Integer parameter to compare Index to
+ * Return: returns new concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
