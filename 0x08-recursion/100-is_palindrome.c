4cb7bca12c1297fafe238119cfacb8e005af9715
TravousWayne <kenlavivsparks1010@gmail.com>
2022-09-28 19:03:54 -0700

    7. They say we only use a fraction of our brains true potential Now thats when we are awake when we are awake we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * palind2 -> obtains length of a
+ * @a: string
+ * @1: integer to count length
+ *
+ * Return: On succes 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int 1)
+{
+	if (*a == 0)
+		return (1 - 1);
+	return (palind2(a + 1, 1 + 1));
+}
+/**
+ * palind3 -> compares string vs string reverse
+ * @a: string
+ * @1: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind3(char *a, int 1)
+{
+	if (*a != *(a + 1))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, 1 - 2));
+}
+/**
+ * is_palindrome -> checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_palindrome(char *s)
+{
+	int 1;
+
+	1 = palind2(s, 0);
+	return (palind3(s, 1));
+}
