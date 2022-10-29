2f6c5d684f1d995215f8fb3016fd23c44d404d44
TravousWayne <kenlavivsparks1010@gmail.com>
2022-10-28 17:13:10 -0700

    11. 98 Battery Street the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
- * 		in order separated by a comma followed by a space.
- * 		@n: The number to begin counting at.
+ *		in order separated by a comma followed by a space.
+ *		@n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
